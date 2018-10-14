fd1fd1d02135fc564120b536345773ed07075f99
pringgojs <odyinggo@gmail.com>
2018-10-14 11:49:30 +0700

    fix table respionsive

--- resources/views/backend/pages/juri/_form-random.blade.php
+++ resources/views/backend/pages/juri/_form-random.blade.php
@@ -1,14 +1,14 @@
 <form id="form-set-juri" name="form_overtime">
     {!! csrf_field() !!}
     <input type="hidden" name="juri_id" value="{{$juri}}">
-    <table id="table" class="table table-bordered table-hover">
+    <table id="table" class="table table-bordered table-hover table-responsive">
         <thead>
             <tr>
                 <th>No</th>
                 <th>Nama Tim</th>
                 <th>Asal PT</th>
-                <th>Proposal</th>
                 <th>Kategori</th>
+                <th>Proposal</th>
             </tr>
         </thead>
         <tbody>
@@ -20,8 +20,8 @@
                 <td>{{ $row++ }}</td>
                 <td id="tim-{{$item->id}}">{{ $item->nama_tim }}</td>
                 <td>{{ $item->politeknik->politeknik }}</td>
-                <td>@if($item->file_proposal) <a href="{{ url('proposal/'.$item->file_proposal) }}">{{ $item->file_proposal }}</a>@endif</td>
                 <td>{{ $item->kategori->kategori }}</td>
+                <td>@if($item->file_proposal) <a href="{{ url('proposal/'.$item->file_proposal) }}">{{ $item->file_proposal }}</a>@endif</td>
             </tr>
             @endforeach
             @elseif($random == 1)
